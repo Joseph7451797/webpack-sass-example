1665b1fbfb7a4b79360a0308d051b240a811027a
Joseph <k7451797@gmail.com>
2016-03-16 22:23:05 +0800

    refactor assets

--- js/example.js
+++ js/example.js
@@ -1,13 +1,3 @@
-var G = require('./global.js');
+// I am a comment, and I will be removed after npm run build
 
-var $div1 = $("#div1"),
-	$div2 = $("#div2"),
-	$div3 = $("#div3");
-
-$div1.find('span').text(G._url());
-$div2.find('span').text(G._testIfMobile());
-$div3.find('span').text(G.windowWidth);
-
-
-G._log($div1);
-G._limit();
+console.log('Only I left!!!'); 
