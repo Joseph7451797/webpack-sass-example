ea203d6cb647e4668242259cc18333ee06762d87
Joseph7451797 <k7451797@gmail.com>
2017-03-12 00:17:54 +0800

    translate README.md to English, merge all webpack setting into one file

--- README.md
+++ README.md
@@ -1,32 +1,35 @@
-# webpack-reload-test
+# webpack-sass-example
 
-支援 SCSS、JS 熱載入，有開發模式與產出（build）模式兩種設定，詳見 webpack-hot-dev-server.config.js 與 webpack.build.config.js
-(原本的 webpack.config.js 已棄用)
+This is an example of sass/scss and webpack integration.
 
-## 使用方法
+## Requirements
+- npm >3
 
-下載後安裝必須模組：
+## How to use
 
-```text
-npm install
+Download or clone this repository, and run:
+```bash
+$ npm install
 ```
 
-啟動開發模式：
-
-```text
-npm run hot-dev
+Open dev mode:
+```bash
+$ npm run hot-dev
 ```
-瀏覽器打開 http://127.0.0.1:8080 可以享受熱載入的功能
 
+Open `http://localhost:8080/`, and you can see basic UI. Now return to your editor and change any sass setting in `scss/partials/index.scss`. After save the file, browser will automatically reload and style changed.
 
-產出模式：
-
-```text
-npm run build
+Host mode:
+```bash
+$ npm run host
 ```
-<br>
-**附註：**
-持續更新中，LESS 熱載入功能遙遙無期
+
+Use host feature of webpack-dev-server. It alllows your colleague to see your web page from LAN. Just give them url address returned in terminal.
+
+Generate bundle：
+```bash
+$ npm run build
+```
 
 ## License
 
---
+++ webpack.config.js
@@ -0,0 +1,87 @@
+var path = require('path');
+var webpack = require('webpack');
+var MODE = '';
+
+switch (process.env.NODE_ENV) { // decide mode
+    case 'hot-dev':
+        MODE = 'dev';
+        break
+    case 'host':
+        MODE = 'host';
+        break
+    case 'build':
+        MODE = 'build';
+        break
+    default:
+        MODE = 'dev';
+        break
+}
+
+var config = function() {
+
+    var publicPath = '';
+
+    if( MODE === 'host' ) {
+        publicPath = 'http://' + process.argv[8] + ':8080/build/';
+    }else if( MODE === 'dev' ) {
+        publicPath = 'http://localhost:8080/build/';
+    }else {
+        publicPath = '/build/';
+    }
+
+    var entry = {
+        main: [
+            "./scss/main.scss"
+        ]
+    };
+
+    var output = {
+        filename: "[name].js",
+        path: __dirname + "/build/",
+        publicPath: publicPath
+    };
+
+    var module = {
+        loaders: [
+            {
+                test : /\.(woff|ttf|svg|eot|jpg|png|git|gif)$/,
+                loader: 'url-loader?limit=8192'
+            },
+            {
+                test   : /\.s(a|c)ss$/,
+                loader :'style-loader!css-loader?sourceMap!sass-loader?includePaths[]='
+                        + path.resolve(__dirname, './node_modules/compass-mixins/lib')
+            }
+        ]
+    };
+
+    var plugins = [];
+
+    if( MODE === 'build' ) {
+        plugins.push(
+            new webpack.optimize.DedupePlugin(),
+            new webpack.optimize.OccurenceOrderPlugin(),
+            new webpack.optimize.AggressiveMergingPlugin(),
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false
+                },
+                comments: false
+            })
+        );
+    }else if( MODE === 'dev' ) {
+        plugins.push(
+            new webpack.NoErrorsPlugin()
+        );
+    }
+
+    return {
+        devtool: MODE === 'dev' || MODE === 'host' ? 'source-map' : false,
+        entry: entry,
+        output: output,
+        module: module,
+        plugins: plugins
+    }
+};
+
+module.exports = config();
